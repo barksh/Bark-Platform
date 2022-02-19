af90986df4fceed96d1f6a1fc3cf389d25447558
WMXPY <wm@mengw.io>
2022-02-18 19:08:55 -0600

    fix linting

--- src/platform.ts
+++ src/platform.ts
@@ -8,10 +8,11 @@ import { BarkPreference } from "@barksh/preference";
 
 export abstract class BarkPlatform {
 
+    public abstract platform: string;
+
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
     protected constructor() {
     }
-
-    public abstract platform: string;
 
     public abstract readBarkPreference(): BarkPreference | Promise<BarkPreference>;
     public abstract writeBarkPreference(preference: BarkPreference): Promise<void>;
